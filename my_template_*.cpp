47985f5bfefd30e177e804282fe524009270ea50
Prateek Agarwal <prrateekk@hotmail.com>
2016-03-23 16:18:22 +0530

    Create my_template_*.cpp

---
+++ my_template_*.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Author      : Prateek Agarwal
+// Institution : NITJ
+//============================================================================
+
+#include <bits/stdc++.h>
+
+#define ll long long
+
+#define TEST int t;scan(t);while(t--)
+#define scan(n) scanf("%d",&n)
+#define scanl(n) scanf("%lld",&n)
+#define set(x,y) memset(x,y,sizeof(x))
+#define loop(i,l,r,x) for (int i=l;i<=r;i+=x)
+#define printl(n) printf("%d\n",n)
+#define print(n) printf("%d ",n)
+#define pb push_back
+
+#define INF 1000000000
+#define M 1000000009
+#define MAX 100000000
+#define LIM 10
+
+using namespace std;
+
+int init(){
+	TEST
+
+	return 0;
+}
+
+int main(){
+	//clock_t start = clock();
+	init();
+	//printf("%.6f\n",double(clock()-start)/CLOCKS_PER_SEC);
+	return 0;
+}
+
